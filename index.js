cddb0967e92297a4d35d1e7e1c458b243e1f77a5
kdrolle594 <kdrolle594@outlook.com>
2026-02-03 14:23:05 -0500

    calculate Total function fixed

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
  const mySmall = document.getElementById("mySmall")
-  const myLarge = document.getElementById("myLarge")
-   const myMedium = document.getElementById("myMedium")
-   const mySausage = document.getElementById("mySausage")
-    const myPepperoni = document.getElementById("myPeppeoni")
-    const myPinapple = document.getElementById("myPineapple")
-     const myOrderButton = document.getElementById("orderButton")
-      const myTotal = document.getElementById("total")
-       const myThanks = document.getElementById("thanks")
+const myLarge = document.getElementById("myLarge")
+const myMedium = document.getElementById("myMedium")
+const mySausage = document.getElementById("mySausage")
+const myPepperoni = document.getElementById("myPepperoni")
+const myPinapple = document.getElementById("myPineapple")
+const myOrderButton = document.getElementById("orderButton")
+const myTotal = document.getElementById("total")
+const myThanks = document.getElementById("thanks")
 let myResult = 0  
 ///Yooooooo
-       myOrderButton.onclick = function calculateTotal(){
-        myResult = 0;
-        if(mySmall.checked)
-        {if(mySausage.checked){myResult += 2 }
-        {if(myPepperoni.checked){myResult += 3 }
-        {if(myPinapple.checked){myResult += 4 }
-            myResult += 6
-         if(myMedium.checked){
-            myResult += 9
-        }
-        if(myLarge.checked) {
-            myResult += 12
-        };
-          
-       }
+function calculateTotal(){
+    myResult = 0;
+    if(mySmall.checked){myResult += 6}
+    if(mySausage.checked){myResult += 2 }
+    if(myPepperoni.checked){myResult += 3 }
+    if(myPinapple.checked){myResult += 4 }
+    if(myMedium.checked){myResult += 9}
+    if(myLarge.checked) {myResult += 12}
 
-    }}}
+    myTotal.innerHTML = "Your total is $" + myResult + ".00"
+    myThanks.innerHTML = "Thank you for your order!"
+
+          
+}
